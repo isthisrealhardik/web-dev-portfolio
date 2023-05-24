3aef93ad809c749d0544c3c59497bc583363bc4e
Hardik <singhhardik6014@gmail.com>
2023-05-24 16:38:19 +0530

    desktop ver is done too

--- src/App.jsx
+++ src/App.jsx
@@ -45,31 +45,49 @@ function App() {
     // container
     <div className='bg-primary h-full w-screen font-prata text-secondary'>
       {/* nav */}
-      <div className='flex justify-between items-center py-6 px-4 md:px-28'>
+        {/* mobile nav */}
+      <div className='flex justify-between items-center py-6 px-4 md:px-28 lg:hidden'>
         <h1 className='text-lg'>Johnathan Specter</h1>
         <img src={Menu} alt="Menu" />
       </div>
+        {/* desktop nav */}
+      <div className='hidden lg:flex justify-between items-center py-6 px-32'>
+        <h1 className='text-sm'>Johnathan Specter</h1>
+        <div className='flex justify-between space-x-4 text-sm font-inter'>
+          <a href="#" className='text-secondary opacity-60 hover:opacity-90 hover:text-secondary' >Articles</a>
+          <a href="#" className='text-secondary opacity-60 hover:opacity-90 hover:text-secondary' >Chats</a>
+          <a href="#" className='text-secondary opacity-60 hover:opacity-90 hover:text-secondary' >Awards</a>
+          <a href="#" className='text-secondary opacity-60 hover:opacity-90 hover:text-secondary' >About</a>
+        </div>
+        <button className='bg-quaternery h-12 rounded-none text-primary font-prata text-base hover:border hover:border-quaternery hover:bg-transparent hover:text-quaternery transition-all'>Get in touch</button>
+      </div>
       {/* part 1 */}
-      <div className='py-6 px-4 flex flex-col md:px-28'>
-        <h1 className='text-3xl leading-[45px] mb-4' >Helping companies build better, scalable software.</h1>
-        <p className='opacity-50 font-inter text-base' >Award-winning web developer and author, with over 15+ years of working experience with Fortune 500 companies like Apple, Google, Facebook, and more.</p>
+      <div className='py-6 px-4 flex flex-col md:px-28 lg:px-32'>
+        <h1 className='text-3xl leading-[45px] mb-4 lg:hidden' >Helping companies build better, scalable software.</h1>
+        <p className='opacity-50 font-inter text-base lg:hidden' >Award-winning web developer and author, with over 15+ years of working experience with Fortune 500 companies like Apple, Google, Facebook, and more.</p>
+        <div className='hidden lg:flex flex-col pr-96 my-4'>
+          <h1 className='text-5xl leading-[60px] mb-4' >Helping companies build better, scalable software.</h1>
+          <p className='opacity-50 font-inter text-lg' >Award-winning web developer and author, with over 15+ years of working experience with Fortune 500 companies like Apple, Google, Facebook, and more.</p>
+        </div>
         {/* logos */}
         <div className='flex flex-wrap my-10 justify-center items-center px-3' >
           {logos.map(logo => (
-            <img src={logo} alt="logo" key={logo} className='w-24 my-3 mx-3' />
+            <img src={logo} alt="logo" key={logo} className='w-24 my-3 mx-3 lg:mx-6 lg:my-4' />
           ))}
         </div>
         {/* gallery */}
-        {gallery.map(obj => (
-          <div key={obj.name} className='flex flex-col my-6'>
-            <img src={obj.image} alt={obj.name} className='object-cover mb-6' />
-            <h1 className='text-3xl mb-3 underline' >{obj.name}</h1>
-            <p className='text-base opacity-50 font-inter'>{obj.description}</p>
-          </div>
-        ))}
+        <div className='lg:flex lg:justify-between lg:items-center lg:space-x-8 '>
+          {gallery.map(obj => (
+            <div key={obj.name} className='flex flex-col my-6 lg:justify-between w-[500px]'>
+              <img src={obj.image} alt={obj.name} className='object-cover mb-6 lg:w-[450px]' />
+              <h1 className='text-3xl mb-3 underline' >{obj.name}</h1>
+              <p className='text-base opacity-50 font-inter lg:text-sm'>{obj.description}</p>
+            </div>
+          ))}
+        </div>
       </div>
       {/* part 2 */}
-      <div className='bg-tertiary my-4 py-12 px-4 font-inter text-base md:px-28'>
+      <div className='bg-tertiary my-4 py-12 px-4 font-inter text-base md:px-28 lg:flex lg:flex-wrap'>
         <h1 className='text-3xl font-prata mb-4' >A co-founder at one of the world's largest communities.</h1>
         <p className='opacity-50 mb-8'>The combined experience I have working at the top Fortune 500 companies has allowed me to develop a skillset that helps me in not just development, but in every aspect of any business.</p>
         <p className='opacity-50 mb-12'>I'm proud to announce that I am now working at one of the world's largest communities teaching young minds about how to self yourself as a developer and open them to a whole new world of opportunites</p>
